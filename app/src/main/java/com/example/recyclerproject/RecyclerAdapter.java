8bdea3b0757ff31ace38f726b4c03262817b9ab4
mohitshahi <mohitshahi238@gmail.com>
2019-11-07 16:23:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recyclerproject/RecyclerAdapter.java
@@ -0,0 +1,48 @@
+package com.example.recyclerproject;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+    private List<String> list;
+    public RecyclerAdapter(List<String> list){
+
+        this.list=list;
+    }
+
+    @NonNull
+    @Override
+
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        TextView textView= (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.text_view_layout,parent,false);
+        MyViewHolder myViewHolder=new MyViewHolder(textView);
+
+
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+holder.VersionName.setText(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+TextView VersionName;
+        public MyViewHolder(TextView itemView) {
+            super(itemView);
+            VersionName=itemView;
+        }
+    }
+}
